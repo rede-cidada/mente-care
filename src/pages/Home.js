1f675be6cc8eb1b66d3135ad3dbc98b68ca9cedf
Luciana Cândido <lucianacscandido@gmail.com>
2020-09-18 21:37:35 -0300

    Adicionando contéudo da Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Background from '../components/backgroundSvg/Background'
-
+import HomeContent from '../components/homeContent/HomeContent'
 
 function App() {
   return (
     <div className="container">
     <Background />
       <Header />
+      <HomeContent />
     </div>
   )
 }
