2a30f24bdfe48119e64b39f179b584f14d1cdc55
freitasrayani <freitas.rayani1@gmail.com>
2020-09-24 20:32:24 -0300

    Pagina error 404

---
+++ src/components/errorPage/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './errorPage.css';
+
+const ErrorPage = () => (
+
+    <div className="container">
+      <h1 className="title">404</h1>
+      <p className="sub-title">Oops! Page not found</p>
+      <p className="txt">We can't seem to find the page you're looking for. </p>
+      <button className="btn">Go back home</button>
+   </div>
+)
+
+export default ErrorPage
