3b6d9097eca10a116108a68ed5734302c217f134
Luciana Cândido <lucianacscandido@gmail.com>
2020-10-03 20:34:28 -0300

    Ligando componente fale conosco a página inicial

--- src/components/FaleConosco/FaleConosco.js
+++ src/components/FaleConosco/FaleConosco.js
@@ -10,8 +10,8 @@ const FaleConosco = () => (
 
     <div className='faleConosco-texts'>
     <h1 className='faleConosco-texts1'>Quer falar com</h1>
-    <h1 className='faleConosco-texts1'>a gente?</h1>
-    <p className='faleConosco-texts2'>É só mandar sua mensagem por aqui ou por nossas redes....</p>
+    <h2 className='faleConosco-texts1'>a gente?</h2>
+    <p className='faleConosco-texts2'>É só mandar sua mensagem por aqui ou por nossas redes.</p>
     <p className='faleConosco-texts2'></p>
     </div>
     <div className='faleConosco-form-area'>
