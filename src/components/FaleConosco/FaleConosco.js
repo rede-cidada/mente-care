b347a4df32966973a9a4682cddfaab6e8d4ededd
Luciana Cândido <lucianacscandido@gmail.com>
2020-10-04 08:11:12 -0300

    Adicionado a validação dos inputs no componente fale conosco

--- src/components/FaleConosco/FaleConosco.js
+++ src/components/FaleConosco/FaleConosco.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './faleConosco.css';
 import Svgs from '../../components/universalComponents/backgroundSvg/Svgs'
 import call from './assets/video-calling.png'
 
-const FaleConosco = () => (
+const FaleConosco = () => {
+ const [valorNome, setvalorN] = useState('')
+ const [valorEmail, setvalorE] = useState('')
+ const [valorCelular, setvalorC] = useState('')
+ const [valorTexto, setvalorT] = useState('')
+
+const [Resposta, setresposta] = useState('faleConosco-Resposta-Mensagem')
+const [Mensagem, setmensagem] = useState('')
+
+ const ValidandoDados = () => {
+    if (valorNome === ''|| valorEmail === ''|| valorCelular === ''|| valorTexto === '') {
+      return (setresposta('faleConosco-Resposta-Mensagem faleConosco-Resposta'),
+              setmensagem('Dados incompletos')
+      )
+    }
+    else {
+      return (setresposta('faleConosco-Resposta-Mensagem faleConosco-Resposta faleConosco-Resposta-Verdade'),
+            setmensagem('Obrigada pela sua mensagem! Em breve entraremos em contato.')
+      )
+    }
+ }
+
+  return (
+
   <div className='container-faleConosco'>
      <Svgs />
 
+    <div className={Resposta}>{Mensagem}</div>
 
     <div className='faleConosco-texts'>
     <h1 className='faleConosco-texts1'><span>Quer falar com a gente?</span></h1>
@@ -17,11 +41,12 @@ const FaleConosco = () => (
     <div className='faleConosco-form-area'>
     <div className='faleConosco-form-area2'>
     <div className='faleConosco-form'>
-    <input className='faleConosco-form-input' type='text' placeholder='Nome completo' />
-    <input className='faleConosco-form-input' type='email' placeholder='Email' />
-    <input className='faleConosco-form-input' type='text' placeholder='Celular' />
-    <textarea rows="5" cols="5" className='faleConosco-form-msg' placeholder="Digite aqui sua mensagem"></textarea>
-    <button  className='faleConosco-form-button'>Enviar</button>
+    <input className='faleConosco-form-input' type='text' placeholder='Nome completo' value={valorNome} onChange={(e) => setvalorN(e.target.value)}
+ />
+    <input className='faleConosco-form-input' type='email' placeholder='Email' value={valorEmail} onChange={(e) => setvalorE(e.target.value)} />
+    <input className='faleConosco-form-input' type='text' placeholder='Celular' value={valorCelular} onChange={(e) => setvalorC(e.target.value)} />
+    <textarea rows="5" cols="5" className='faleConosco-form-msg' placeholder='Digite aqui sua mensagem' value={valorTexto} onChange={(e) => setvalorT(e.target.value)}></textarea>
+    <button  className='faleConosco-form-button' onClick={ValidandoDados}>Enviar</button>
     </div>
     
     </div>
@@ -31,6 +56,8 @@ const FaleConosco = () => (
    
     </div>
     </div>
-);
+    
+    
+); }
 
 export default FaleConosco;
