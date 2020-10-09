096acc36316a4b593ab319cbd1d3bc38c8066bd2
rap0so <william.s.godoy53@gmail.com>
2020-10-09 00:09:15 -0300

    Extraindo rotas do componente Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,75 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+
 import Header from '../../components/universalComponents/header/Header'
-import HomeContent from '../../components/homeContent/HomeContent'
-import About from '../../components/about/About'
 import Footer from '../../components/universalComponents/footer/Footer'
-import AboutUs from '../../components/aboutUs/aboutUs'
+import Routes from '../../routes'
+
 import './home.css'
-import ErrorPage from '../../components/errorPage/ErrorPage'
-import Login from '../../components/login/Login'
-import Cadastro from '../../components/cadastro/Cadastro'
-import Modal from '../../components/modal/Modal'
-import CadastroPaciente from '../../components/CadastroPaciente/CadastroPaciente'
-import FaleConosco from '../../components/FaleConosco/FaleConosco'
-import List from '../../components/list/list'
 
 function App() {
   return (
     <BrowserRouter>
-      < div className="container">
+      <div className="container">
         <Header />
-        
-        <Switch>
-          <Route exact path="/">
-            <HomeContent />
-          </Route>
-
-          <Route exact path="/about">
-            <About />
-          </Route>
-          
-          <Route exact path="/aboutUs">
-            <AboutUs />
-          </Route>
-    
-          <Route exact path="/cadastro">
-            <Cadastro />
-          </Route>
-         
-         
-          <Route exact path="/modal">
-            <Modal />
-          </Route>
-         
-         
-          <Route exact path="/cadastroPaciente">
-            <CadastroPaciente />
-          </Route>
-         
-          <Route exact path="/faleConosco">
-            <FaleConosco />
-          </Route>
-         
-          <Route exact path="/list">
-            <List />
-          </Route>
-         
-          <Route exact path="/entrar">
-            <Login />
-          </Route>
-
-          <Route >
-            <ErrorPage />
-          </Route>
-
-        </Switch>        
-        
+          <Routes/>
         <Footer />
       </div>
-
     </BrowserRouter>
-
   )
 }
 
