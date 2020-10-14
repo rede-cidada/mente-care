78bfa8491c58e55cdfcc5e9dd71bd8aae4cb33e5
freitasrayani <freitas.rayani1@gmail.com>
2020-10-13 21:26:34 -0300

    adiçao de componentes do site

--- src/components/site/ansiedade/Ansiedade.js
+++ src/components/site/ansiedade/Ansiedade.js
@@ -1,21 +1,13 @@
 import React from "react";
-import './LandingPage.css';
-import clock from './assets/img-ansiedade/clock.jpg'
-import garotanervosa from './assets/img-ansiedade/garota-nervosa.jpg'
-import homemtrabalhando from './assets/img-ansiedade/homem-trabalhando.png'
-import meninoestressado from './assets/img-ansiedade/menino-estressado.jpg'
+import './ansiedade.css';
+import clock from './img-ansiedade/clock.jpg';
+import garotanervosa from './img-ansiedade/garota-nervosa.jpg';
+import homemtrabalhando from './img-ansiedade/homem-trabalhando.png';
+import meninoestressado from './img-ansiedade/menino-estressado.jpg'
 
-const Landing = () => (
+const Ansiedade = () => (
     <div className="landing-content">
-    <section className="slider-section">
-      <div className="sliderBx">
-       
-        
 
-
-      </div>
-
-    </section>
     <section className="info-section1">
       <div className="ansiedade-info">
         <h1 className="info1">Transtorno de Ansiedade</h1>
@@ -93,4 +85,4 @@ const Landing = () => (
 
 
 
-export default Landing;
+export default Ansiedade;
